ac546eda133ee86efac83705734588cb83b9718a
Arthur Sampaio Laurindo <159933584+ArthurSmpio1@users.noreply.github.com>
2025-03-25 00:14:55 -0300

    ajustes no código

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
 function alterarStatus(id){
 
-    let ligado = ['dashboard__item__button dashboard__item__button', 'dashboard__item__img', 'dashboard__item__button']
-    let desligado = ['dashboard__item__button dashboard__item__button--return', 'dashboard__item__img', 'dashboard__item__button dashboard__item__button--return']
+    let alugar = ['dashboard__item__img', 'dashboard__item__name', 'dashboard__item__button', 'Alugar']
+    let devolver = ['dashboard__item__img dashboard__item__img--rented', 'dashboard__item__name', 'dashboard__item__button dashboard__item__button--return', 'Devolver']
+    let jogoEscolhido = document.getElementById(`game-${id}`);
+    let anteriorAlteracao = jogoEscolhido.children[2].className;
 
-    let botaoPressionado = document.getElementById(`game-${id}`);
+    let onOff = jogoEscolhido.children[2].textContent  = 'Alugar' ? devolver : alugar;
+    //alugar e devolver
 
-    console.log(botaoPressionado.children.item(0));
+    console.log(onOff);
 
-    if (botaoPressionado.children[2].className = ligado){
-        botaoPressionado.children[2].className = desligado;
-    }
+    jogoEscolhido.children[0].className = onOff[0];
+    jogoEscolhido.children[2].className = onOff[2];
+    jogoEscolhido.children[2].textContent = onOff[3];
+
+    //status
+
+    console.log(`o valor da classe anteriormente era de ${anteriorAlteracao},
+     se tornando: ${jogoEscolhido.children[2].className}`);
 }
+
+//primeiro filho 0: imagem m
+//segundo filho 1: nome nm
+//terceiro filho 2: botao m
